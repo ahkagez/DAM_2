00d1500af1429dd0a31afcd48660f3e1066f057d
ahkagez <153426933+ahkagez@users.noreply.github.com>
2025-09-24 21:11:46 +0200

    noche

--- refresh/src/Main.java
+++ refresh/src/Main.java
@@ -1,17 +1,26 @@
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Main{
     public static void main(String[] args) {
-        LocalDate fechaInicio = LocalDate.now();
-        LocalDate fechaFin = LocalDate.of(2026, 4, 25);
+        LlibreDigital llibreDigital = new LlibreDigital( "3837282i", true, "Jordi Bosch", "Mentiras", 2024);
+        LlibreFisic llibreFisic = new LlibreFisic("897485i", 10, "Jimena Josees", "No", 2023);
 
-        calcularPrecio(fechaInicio, fechaFin);
+        Usuario usuario = new Usuario("Jordi", "Bosch", "48682658f");
 
-    }
+        LocalDate fechaIn = LocalDate.now();
+        LocalDate fechaOut = LocalDate.of(2025, 11, 04);
 
-    public static void calcularPrecio(LocalDate fechaInicio, LocalDate fechaFin){
-        int mes = fechaInicio.until(fechaFin).getMonths();
-        int dias = fechaInicio.until(fechaFin).getDays();
-        System.out.println(dias + (mes * 30));
+        Prestamo prestamo1 = new Prestamo(llibreDigital, usuario, fechaIn, fechaOut, false);
+
+        Biblioteca biblioteca = new Biblioteca();
+
+        System.out.println(biblioteca);
+        biblioteca.registrarPrestamo(prestamo1);
+        System.out.println(biblioteca);
+
+        biblioteca.comprobarDisponibilidad(llibreDigital);
     }
 }

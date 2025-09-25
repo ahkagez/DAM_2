e3144c77fd891591fff20ce42249dd40d3e74830
ahkagez <ahkagez@gmail.com>
2025-09-25 13:56:39 +0200

    1
    1

--- AC_tema1/src/Main.java
+++ AC_tema1/src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,13 +13,38 @@ public class Main {
             System.out.println("Error");
         }
 
-        try(BufferedReader br = new BufferedReader( new FileReader("C:\\Users\\Jordi B\\IdeaProjects\\AC_tema1\\ad_bosch_ud1_activitat1.txt") )){
-            String linea;
-            while( (linea = br.readLine()) != null ){
-                System.out.println(linea);
+
+
+
+        System.out.println("El contenido del documento de texto es: ");
+        try{
+            FileReader fr = new FileReader("ad_bosch_ud1_activitat1.txt");
+            int i = fr.read();
+            while(i > -1){
+                System.out.print((char)i);
+                i = fr.read();
             }
         }catch(IOException e){
-            System.out.println(e);
+            System.out.println("Error");
+        }
+
+
+
+
+
+        try{
+            RandomAccessFile raf = new RandomAccessFile("C:\\Users\\Jordi B\\IdeaProjects\\AC_tema1\\ad_bosch_ud1_activitat1.txt", "rw");
+            byte[] bytes = new byte[100];
+            long longitud = raf.length();
+
+            raf.seek(longitud);
+            raf.write("\nActivitat 3 de Jordi Bosch Ciejka".getBytes());
+
+            raf.seek(longitud);
+            raf.read(bytes);
+            System.out.println(new String(bytes).replace("\0", ""));
+        }catch(IOException e){
+            System.out.println("Error");
         }
     }
 }
